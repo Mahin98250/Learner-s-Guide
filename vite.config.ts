1e566f886d2dfd828c7217a62daf539927eb71c9
Mahin98250 <patelmahin140@gmail.com>
2026-06-03 20:23:47 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Learner-s-Guide/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    port: 5173,
-    strictPort: false,
   },
 })
